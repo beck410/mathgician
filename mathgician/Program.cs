2a97c7a58eb1d4cb898abc4e9e89d5198399f2d9
beck410 <beck410@hotmail.com>
2015-01-16 15:42:17 -1200

    initial mathmagian code

---
+++ mathgician/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mathgician {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("I am the MATHGICIAN");
+      Console.WriteLine("What shall I math?\nFibonacci, Primes or Integers?");
+      string desiredMaths = Console.ReadLine();
+      Console.WriteLine("Ok i'm gonna do some " + desiredMaths);
+      if (desiredMaths == "Integers") {
+
+      }
+      else {
+        Console.WriteLine("I lied");
+
+      }
+
+
+      System.Console.WriteLine("Press any key...");
+      System.Console.ReadKey();
+    
+    }
+  }
+}
